93898cd99c544601a6c52cf0bcf5fd84160271d2
iluvyw <phaman.tech@gmail.com>
2023-12-04 22:44:31 +0700

    day 2 part 2

--- day-2/main.go
+++ day-2/main.go
@@ -25,6 +25,23 @@ func isAvailable(row string) bool {
 	return Max["red"] <= 12 && Max["green"] <= 13 && Max["blue"] <= 14
 }
 
+func power(row string) int {
+	splitResult := strings.Split(row, ": ")
+	picks := splitResult[1]
+	Max := make(map[string]int)
+	for _, pick := range strings.Split(picks, "; ") {
+		for _, item := range strings.Split(pick, ", ") {
+			data := strings.Split(item, " ")
+			quantity, _ := strconv.Atoi(data[0])
+			color := data[1]
+			if quantity > Max[color] {
+				Max[color] = quantity
+			}
+		}
+	}
+	return Max["red"] * Max["green"] * Max["blue"]
+}
+
 func main() {
 	content, err := os.ReadFile("input.txt")
 
@@ -34,11 +51,14 @@ func main() {
 
 	stringContent := string(content)
 	rows := strings.Split(stringContent, "\n")
-	sum := 0
+	sumPart1 := 0
+	sumPart2 := 0
 	for i, row := range rows {
 		if isAvailable(row) {
-			sum += i + 1
+			sumPart1 += i + 1
 		}
+		sumPart2 += power(row)
 	}
-	fmt.Printf("Part 1 Result: %v\n", sum)
+	fmt.Printf("Part 1 Result: %v\n", sumPart1)
+	fmt.Printf("Part 2 Result: %v\n", sumPart2)
 }
